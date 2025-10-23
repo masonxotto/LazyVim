1bdeed3e50d81d99dfba8f2c87c9943d9c2ad487
masonotto <mason.otto@hendersonengineers.com>
2025-10-23 08:34:57 -0500

    :sparkles: theme, options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,5 @@ vim.o.shellcmdflag =
   "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
 vim.o.shellredir = "2>&1 | Out-File %s; exit $LastExitCode"
 vim.o.shellpipe = "2>&1 | Out-File %s; exit $LastExitCode"
+
+vim.g.snacks_animate = false
